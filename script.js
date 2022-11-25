714dbc0b65a403707ba2d370cddeb800bbf0b9a3
vijayalakshmi <mamillavijayalakshmi789@gmail.com>
2022-11-25 15:55:04 +0530

    ok

---
+++ script.js
@@ -0,0 +1,13 @@
+ const show=()=>{
+    console.log('hello')
+    document.getElementById('home').style.display="flex"
+}
+
+
+
+
+
+const cls=()=>{
+    console.log('world')
+    document.getElementById('home').style.display="none"
+}
